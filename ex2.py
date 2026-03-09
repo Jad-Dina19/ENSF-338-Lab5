f6487e3c3a65205b208add714d01080f4ada21d1
Fadi <fadi.salman@ucalgary.ca>
2026-03-09 12:36:27 -0600

    Finished questions 1-3 of exercise 2, halfway through question 4

---
+++ ex2.py
@@ -0,0 +1,100 @@
+import random
+from timeit import timeit
+import matplotlib.pyplot as plt
+
+class PriorityQueue1:
+    def __init__(self):
+        self.data = []
+
+    def enqueue(self, data):
+        self.data.append(data)
+        self.merge_sort(0, len(self.data) - 1)
+
+    def dequeue(self):
+        if (len(self.data) == 0):
+            return None
+        return self.data.pop(0)
+    
+    def merge(self, low, mid, high):
+        i = low
+        j = mid + 1
+        temp = []
+
+        while (i <= mid and j <= high):
+            if(self.data[i] <= self.data[j]):
+                temp.append(self.data[i])
+                i+=1
+            else:
+                temp.append(self.data[j])
+                j+=1
+    
+        while (i <= mid):
+            temp.append(self.data[i])
+            i+=1
+        while (j <= high):
+            temp.append(self.data[j])
+            j+=1
+
+        for k in range(len(temp)):
+            self.data[low + k] = temp[k]
+
+    def merge_sort(self, low, high):
+        if(low < high):
+            mid = (low+high)//2
+            self.merge_sort(low, mid)
+            self.merge_sort(mid+1, high)
+            self.merge(low, mid, high)
+
+class PriorityQueue2:
+    def __init__(self):
+        self.data = []
+    
+    def enqueue(self, data):
+        index = 0
+
+        while (index < len(self.data) and data < self.data[index]):
+            index += 1
+
+        self.data.insert(index, data)
+        
+
+    def dequeue(self):
+        if (len(self.data) == 0):
+            return None
+        return self.data.pop(0)
+
+
+def random_task(n):
+    choice = ["enqueue","dequeue"]              # 0 = enqueue and 1 = dequeue
+    probability = [0.7, 0.3]
+    task = random.choices(choice, weights=probability, k=n)
+    return task
+
+def measure(task, queue):
+    return 1
+
+
+def main():
+    queue1 = PriorityQueue1()
+    queue2 = PriorityQueue2()
+
+    queue1_time = []
+    queue2_time = []
+    for _ in range(100):
+        task = random_task(1000)
+        queue1_time_temp = 0
+        queue2_time_temp = 0
+        
+        for i in task:
+            queue1_time_temp += measure(task, queue1)
+            queue2_time_temp += measure(task, queue2)
+
+        queue1_time.append(queue1_time_temp)
+        queue2_time.append(queue2_time_temp)
+    
+    print(queue1_time)
+
+
+
+if __name__ == "__main__":
+    main()
