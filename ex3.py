95a71e35a8cf303ccdd8deadfdc016a79ad6a0af
Fadi Salman Desktop <fadi.salman2006@gmail.com>
2026-03-15 16:51:01 -0600

    Should be done from my end. I did both a Python array implementation and a manual dynamic array implementation and discussed both results in my answers

--- ex3.py
+++ ex3.py
@@ -25,9 +25,10 @@ class StackLinkedList:
     def pop(self):
         if (self.head == None):
             return None
-        wanted = self.head
-        self.head = self.head.next
-        return wanted.data
+        else:
+            wanted = self.head
+            self.head = self.head.next
+            return wanted.data
 
 
 class StackArray:
@@ -36,11 +37,47 @@ class StackArray:
     
     def push(self, data):
         self.data.append(data)
+        
 
     def pop(self):
         if (len(self.data) == 0):
             return None
         return self.data.pop()
+    
+class StackDynamicArray:
+    def __init__(self):
+        self.capacity = 1
+        self.size = 0
+        self.data = [] * self.capacity
+
+    def push(self, data):
+        if (data == None):
+            return
+        self.size += 1
+        new_stack = [None] * self.size
+        
+        for i in range(0, self.size-1):
+            new_stack[i] = self.data[i]
+        new_stack[-1] = data
+
+        self.data = new_stack
+    
+    def pop(self):
+        if (self.size == 0):
+            return None
+        self.size -= 1
+        new_stack = [None] * self.size
+
+        for i in range(0, self.size-1):
+            new_stack[i] = self.data[i]
+        
+        element = self.data[self.capacity-1]
+        self.data = new_stack
+
+        return element
+
+        
+
     
 def random_task(n):
     choice = ["push","pop"]
@@ -60,20 +97,20 @@ def measure(tasks, stack):
     return stack_time
 
 def main():
-    stack_array = []
     stack_list = []
+    stack_array_python = []
 
     for _ in range(100):
         tasks = random_task(10000)
-        array = StackArray()
         linked_list = StackLinkedList()
+        array = StackArray()
 
-        stack_array.append(measure(tasks, array))
         stack_list.append(measure(tasks, linked_list))
+        stack_array_python.append(measure(tasks, array))
     
     plt.figure()
-    plt.hist(stack_array, alpha=0.5, label="Stack Array using python implemenation")
-    plt.hist(stack_list, alpha=0.5, label="Stack Linked List")
+    plt.hist(stack_array_python, bins=20, alpha=0.5, label="Stack Array using python implemenation")
+    plt.hist(stack_list, bins=20, alpha=0.5, label="Stack Linked List")
     plt.title("Comparing Stack Array vs Stack Linked List")
     plt.xlabel("Execution Time")
     plt.ylabel("Frequency")
@@ -81,9 +118,40 @@ def main():
     plt.show()
 
 
+def side():
+    stack_array = []
+    stack_list = []
+
+    for _ in range(100):
+        tasks = random_task(10000)
+        dynamicArray = StackDynamicArray()
+        linked_list = StackLinkedList()
+
+
+        stack_array.append(measure(tasks, dynamicArray))
+        stack_list.append(measure(tasks, linked_list))
+
+    
+    plt.figure()
+    plt.hist(stack_array, bins=20, alpha=0.5, label="Stack Array using manual implemenation")
+    plt.hist(stack_list, bins=20, alpha=0.5, label="Stack Linked List")
+    plt.title("Comparing Stack Array vs Stack Linked List")
+    plt.xlabel("Execution Time")
+    plt.ylabel("Frequency")
+    plt.legend()
+    plt.show()
 
 if (__name__ == '__main__'):
     main()
+    # side()
 
-            
+''' # Question 5
+The results indicate that a stack array implementation is more efficient than a stack linked list. This is due to a 
+couple of reasons. Python lists are implemented dynamically in a really efficient way using optimized C code, making
+append() and pop() very efficient. To observe the difference I implemented my own dynamic arrays and seeing how much
+worse the execution time is. You can see the results by uncommenting side(). The list implementation also requires a
+new node object to be created each time and pointer reassignment with push() and pop(). This means that the array 
+implementation should have faster execution times.
+
+'''     
     
